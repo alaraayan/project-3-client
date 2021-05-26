f96e530796fc17311a8472e9e02ce20a7e0a833f
Chloe Brown <chloemail@protonmail.com>
2021-05-26 10:42:17 +0100

    add a film component almost done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import NewMovie from './components/NewMovie'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1>New Movie</h1>
+      <NewMovie />
+    </>
+  )
 }
 
 export default App
