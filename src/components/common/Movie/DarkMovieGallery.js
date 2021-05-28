9dd71316e98c7be1ca499adefd450899ce3b6eeb
Chloe Brown <chloemail@protonmail.com>
2021-05-28 16:56:47 +0100

    nav bar styling updates again, gallery components

---
+++ src/components/common/Movie/DarkMovieGallery.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function DarkMovieGallery() {
+
+  return (
+    <>
+      <section className="mood-gallery-container">
+        <h1>Dark Movies</h1>
+        <figure>posters to show here</figure>
+      </section>
+    </>
+  )
+}
+
+export default DarkMovieGallery
---
+++ src/components/common/Movie/LolMovieGallery.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function LolMovieGallery() {
+
+  return (
+    <>
+      <section className="mood-gallery-container">
+        <h1>LOL Movies</h1>
+        <figure>posters to show here</figure>
+      </section>
+    </>
+  )
+}
+
+export default LolMovieGallery
