ef9074af70cd8b06fa03503fbd1b70594e764a4b
alara <alaraayan@hotmail.com>
2022-02-13 21:18:04 +0000

    fixed camel case

--- src/components/common/NotAuthorized.js
+++ src/components/common/NotAuthorized.js
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom'
 function NotAuthorized() {
   const quotes = [
-    [{ moviename: 'Jaws', character: 'Martin Brody', quote: '\'You\'re gonna need a bigger boat\'' }],
-    [{ moviename: 'The Wizard of Oz', character: 'Dorothy Gale', quote: '\'Toto, I\'ve got a feeling we\'re not in Kansas anymore\'' }],
-    [{ moviename: 'Apollo 13', character: 'Jim Lovell', quote: 'Houston, we have a problem' }],
-    [{ moviename: 'Forrest Gump', character: 'Forrest', quote: '\'Mama always said life was like a box of chocolates. You never know what you\'re gonna get\'' }],
-    [{ moviename: 'A Few Good Men', character: 'Col. Nathan R. Jessup', quote: '\'You can\'t handle the truth!\'' }],
-    [{ moviename: 'Dirty Dancing', character: 'Johnny Castle', quote: '\'Nobody puts Baby in a corner\'' }],
-    [{ moviename: 'Se7en (1995)', character: 'Detective David Mills', quote: '\'What\'s on the page?!\'' }],
-    [{ moviename: 'Fight Club (1999)', character: 'The Narrator', quote: '\'I am Jack\'s missing page\'' }],
-    [{ moviename: 'Star Wars: Episode IV - A New Hope (1977)', character: 'Obi-Wan Kenobi', quote: '\'This is not the webpage you\'re looking for.\'' }]
+    [{ movieName: 'Jaws', character: 'Martin Brody', quote: '\'You\'re gonna need a bigger boat\'' }],
+    [{ movieName: 'The Wizard of Oz', character: 'Dorothy Gale', quote: '\'Toto, I\'ve got a feeling we\'re not in Kansas anymore\'' }],
+    [{ movieName: 'Apollo 13', character: 'Jim Lovell', quote: 'Houston, we have a problem' }],
+    [{ movieName: 'Forrest Gump', character: 'Forrest', quote: '\'Mama always said life was like a box of chocolates. You never know what you\'re gonna get\'' }],
+    [{ movieName: 'A Few Good Men', character: 'Col. Nathan R. Jessup', quote: '\'You can\'t handle the truth!\'' }],
+    [{ movieName: 'Dirty Dancing', character: 'Johnny Castle', quote: '\'Nobody puts Baby in a corner\'' }],
+    [{ movieName: 'Se7en (1995)', character: 'Detective David Mills', quote: '\'What\'s on the page?!\'' }],
+    [{ movieName: 'Fight Club (1999)', character: 'The Narrator', quote: '\'I am Jack\'s missing page\'' }],
+    [{ movieName: 'Star Wars: Episode IV - A New Hope (1977)', character: 'Obi-Wan Kenobi', quote: '\'This is not the webpage you\'re looking for.\'' }]
   ]
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
   return (
@@ -18,7 +18,7 @@ function NotAuthorized() {
         {randomQuote.map(quoteDetails => {
           return (
             <>
-              <div className="quotes-container" key={quoteDetails.moviename}>
+              <div className="quotes-container" key={quoteDetails.movieName}>
                 <figure className="quote">
                   <div className="error-container">
                     <h1>401 <br /> ERROR</h1>
@@ -30,7 +30,7 @@ function NotAuthorized() {
                   </div>
                 </figure>
                 <figcaption>
-                  <p> &mdash;{quoteDetails.character}, <cite>{quoteDetails.moviename}</cite></p>
+                  <p> &mdash;{quoteDetails.character}, <cite>{quoteDetails.movieName}</cite></p>
                 </figcaption>
               </div>
               <p>Unauthorized request! Please <Link className="homepage-link" to="/login">Login</Link> or <Link className="homepage-link" to="/register">Register</Link> in order to proceed. Otherwise, you can head back to the <Link className="homepage-link" to="/">homepage</Link>.</p>
--- src/components/common/NotFound.js
+++ src/components/common/NotFound.js
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom'
 function NotFound() {
   const quotes = [
-    [{ moviename: 'Jaws', character: 'Martin Brody', quote: '\'You\'re gonna need a bigger boat\'' }],
-    [{ moviename: 'The Wizard of Oz', character: 'Dorothy Gale', quote: '\'Toto, I\'ve got a feeling we\'re not in Kansas anymore\'' }],
-    [{ moviename: 'Apollo 13', character: 'Jim Lovell', quote: '\'Houston, we have a problem\'' }],
-    [{ moviename: 'Forrest Gump', character: 'Forrest', quote: '\'Mama always said life was like a box of chocolates. You never know what you\'re gonna get\'' }],
-    [{ moviename: 'A Few Good Men', character: 'Col. Nathan R. Jessup', quote: '\'You can\'t handle the truth!\'' }],
-    [{ moviename: 'Dirty Dancing', character: 'Johnny Castle', quote: '\'Nobody puts Baby in a corner\'' }],
-    [{ moviename: 'Se7en (1995)', character: 'Detective David Mills', quote: '\'What\'s on the page?!\'' }],
-    [{ moviename: 'Fight Club (1999)', character: 'The Narrator', quote: '\'I am Jack\'s missing page\'' }],
-    [{ moviename: 'Star Wars: Episode IV - A New Hope', character: 'Obi-Wan Kenobi', quote: '\'This is not the webpage you\'re looking for.\'' }]
+    [{ movieName: 'Jaws', character: 'Martin Brody', quote: '\'You\'re gonna need a bigger boat\'' }],
+    [{ movieName: 'The Wizard of Oz', character: 'Dorothy Gale', quote: '\'Toto, I\'ve got a feeling we\'re not in Kansas anymore\'' }],
+    [{ movieName: 'Apollo 13', character: 'Jim Lovell', quote: '\'Houston, we have a problem\'' }],
+    [{ movieName: 'Forrest Gump', character: 'Forrest', quote: '\'Mama always said life was like a box of chocolates. You never know what you\'re gonna get\'' }],
+    [{ movieName: 'A Few Good Men', character: 'Col. Nathan R. Jessup', quote: '\'You can\'t handle the truth!\'' }],
+    [{ movieName: 'Dirty Dancing', character: 'Johnny Castle', quote: '\'Nobody puts Baby in a corner\'' }],
+    [{ movieName: 'Se7en (1995)', character: 'Detective David Mills', quote: '\'What\'s on the page?!\'' }],
+    [{ movieName: 'Fight Club (1999)', character: 'The Narrator', quote: '\'I am Jack\'s missing page\'' }],
+    [{ movieName: 'Star Wars: Episode IV - A New Hope', character: 'Obi-Wan Kenobi', quote: '\'This is not the webpage you\'re looking for.\'' }]
   ]
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
   return (
@@ -22,14 +22,14 @@ function NotFound() {
         {randomQuote.map(quoteDetails => {
           return (
             <>
-              <div className="error-container" key={quoteDetails.moviename}>
+              <div className="error-container" key={quoteDetails.movieName}>
                 <figure className="quote-container">
                   <div className="error">
                     <h1 className="error-text"><span className="error-code">404</span><br/> error</h1>
                   </div>
                   <blockquote>
                     <p className="quote">{quoteDetails.quote}</p>
-                    <p className="quote quote-caption"> &mdash;{quoteDetails.character}, <cite>{quoteDetails.moviename}</cite></p>
+                    <p className="quote quote-caption"> &mdash;{quoteDetails.character}, <cite>{quoteDetails.movieName}</cite></p>
                   </blockquote>
                 </figure>
               </div>
